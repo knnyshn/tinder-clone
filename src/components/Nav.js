b771d77846cc439c431cf13df5d160ae0d0c380a
Kenny <knnyshn@gmail.com>
2023-05-19 14:44:53 -0400

    Auth Modal

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,21 @@ import React from "react";
 import whiteLogo from "../images/tinder_logo_white.png";
 import colorLogo from "../images/color-logo-tinder.png";
 
-function Nav({ minimal, authToken }) {
-
+function Nav({ minimal, authToken, setShowModal, showModal, setIsSignUp }) {
+  const handleClick = () => {
+    setShowModal(true);
+    setIsSignUp(false)
+  };
   return (
     <nav>
       <div className="logo-container">
         <img className="logo" src={minimal ? colorLogo : whiteLogo} alt="" />
-          </div>
-          {!authToken && !minimal && <button className="nav-button">Log in</button>}
+      </div>
+      {!authToken && !minimal && (
+        <button className="nav-button" onClick={handleClick} disabled={showModal}>
+          Log in
+        </button>
+      )}
     </nav>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,23 +5,30 @@ import Authmodal from "../components/Authmodal";
 
 function Home() {
   const [showModal, setShowModal] = useState(false);
-
+  const [isSignUp, setIsSignUp] = useState(true);
   const authToken = false;
 
   const handleClick = () => {
     console.log("clicked");
-    setShowModal(true);
+      setShowModal(true);
+      setIsSignUp(true)
   };
   return (
     <div className="overlay">
-      <Nav minimal={false} authToken={authToken} setShowModal={ setShowModal } showModal={ showModal } />
+      <Nav
+        minimal={false}
+        authToken={authToken}
+        setShowModal={setShowModal}
+        showModal={showModal}
+        setIsSignUp={setIsSignUp}
+      />
       <div className="home">
-        <h1>Swipe Right</h1>
+        <h1 className="primary-title">Swipe Right</h1>
         <button className="primary-button" onClick={handleClick}>
           {authToken ? "Signout" : "Create Account"}
         </button>
 
-        {showModal && <Authmodal setShowModal={setShowModal} />}
+        {showModal && <Authmodal setShowModal={setShowModal} isSignUp={isSignUp}/>}
       </div>
     </div>
   );
