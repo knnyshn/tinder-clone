e52caa8e0ed55e842b98914d37d2a8bf664a32ce
Kenny <knnyshn@gmail.com>
2023-06-10 10:11:00 -0400

    Removing env

--- client/src/axios.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-export const instance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://tinder-clone-back.herokuapp.com' : 'http://localhost:8000'
-})
